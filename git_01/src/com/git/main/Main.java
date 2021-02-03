924bfab8732ae26aa4bbf2acc1f78bb2660f100f
gilbeee <78468421+gilbeee@users.noreply.github.com>
2021-02-03 17:19:43 +0900

    Update Main.java

--- git_01/src/com/git/main/Main.java
+++ git_01/src/com/git/main/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		System.out.println("Hello Hello~");
 		System.out.println("hahaha...");
 		System.out.println("Hello JQuery!");
+		System.out.println("Hello Javascript!");
 	}
 
 }
